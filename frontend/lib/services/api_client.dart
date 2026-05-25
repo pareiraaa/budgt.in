3697e30a3d2d4eba944f6999612dbe65e5201203
Farhan Alvin Pareira <farhanalvinpareira@Farhans-MacBook-Pro.local>
2026-05-25 23:14:32 +0700

    update base url to production

--- frontend/lib/services/api_client.dart
+++ frontend/lib/services/api_client.dart
@@ -1,7 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiClient {
-  static const String baseUrl = 'http://10.0.2.2:3000'; //android
+  static const String baseUrl = 'https://serene-passion-production-7799.up.railway.app'; //railway
+  // static const String baseUrl = 'http://10.0.2.2:3000'; //android
   // static const String baseUrl = 'http://localhost:3000'; //web
 
   //token
